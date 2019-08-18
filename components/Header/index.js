c53947302bd83a90107ac43cb5a280c45422ef1d
Kyle Leonard <kdleonard@icloud.com>
2019-08-18 18:33:21 -0500

    Completed Header File

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,27 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+  let header = document.createElement("div");
+  header.classList.add("header");
+
+  let date = document.createElement("span");
+  date.classList.add("date");
+  date.textContent = "MARCH 28, 2019";
+
+  let h1 = document.createElement("h1");
+  h1.textContent = " Lambda Times";
+
+  let temp = document.createElement("span");
+  temp.classList.add("temp");
+
+  header.appendChild(date);
+  header.appendChild(h1);
+  header.appendChild(temp);
+
+  return header;
+}
+
+let container = document.querySelector(".header-container");
+
+container.appendChild(Header());
