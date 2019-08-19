d2f7194deb60568396090dbc5f8291a536b6aa3e
Kyle Leonard <kdleonard@icloud.com>
2019-08-18 19:46:20 -0500

    Completed Cards File

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,45 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardsContainer = document.querySelector(".cards-container");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    let articleArray = response.data.articles;
+    for (let article in articleArray) {
+      articleArray[article].map(item => {
+        cardsContainer.appendChild(newCards(item));
+      });
+    }
+  });
+
+function newCards(data) {
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  let headline = document.createElement("div");
+  headline.classList.add("headline");
+  headline.textContent = data.headline;
+
+  let author = document.createElement("div");
+  author.classList.add("author");
+
+  let imageContainer = document.createElement("div");
+  imageContainer.classList.add("img-container");
+
+  let image = document.createElement("img");
+  image.src = data.authorPhoto;
+
+  let span = document.createElement("span");
+  span.textContent = `By ${data.authorName}`;
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imageContainer);
+  imageContainer.appendChild(image);
+  author.appendChild(span);
+
+  return card;
+}
